3d6571186669aab0889952f271630a8373568bc1
Kyaw Aung Win <kyawagwin@gmail.com>
2017-07-25 17:52:56 +0800

    onActivityResult : capture choose photo

--- app/src/main/java/com/passioncreativestudio/kyawagwin/myapplication/ImagePickActivity.java
+++ app/src/main/java/com/passioncreativestudio/kyawagwin/myapplication/ImagePickActivity.java
@@ -2,12 +2,17 @@ package com.passioncreativestudio.kyawagwin.myapplication;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class ImagePickActivity extends AppCompatActivity implements View.OnClickListener {
     private static final int IMAGE_PICK_REQUEST_CODE = 100;
@@ -33,12 +38,36 @@ public class ImagePickActivity extends AppCompatActivity implements View.OnClick
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
+
         startActivityForResult(intent, IMAGE_PICK_REQUEST_CODE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // TODO: 7/25/2017 capture choose photo
-        super.onActivityResult(requestCode, resultCode, data);
+        InputStream steam = null;
+        if(IMAGE_PICK_REQUEST_CODE == requestCode && resultCode == RESULT_OK) {
+            try {
+                if(mBitmap != null) {
+                    mBitmap.recycle();
+                }
+
+                steam = getContentResolver().openInputStream(data.getData());
+
+                mBitmap = BitmapFactory.decodeStream(steam);
+
+                mImageView.setImageBitmap(mBitmap);
+
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+            } finally {
+                if(steam != null) {
+                    try {
+                        steam.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
     }
 }
